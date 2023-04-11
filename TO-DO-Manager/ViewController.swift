563ed4b562303c3b6aed8785c114a0d61d9c3d6b
Kazimir Makaranka <m2014ky@gmail.com>
2023-04-11 11:18:46 +0300

    Initial Commit

---
+++ TO-DO-Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TO-DO-Manager
+//
+//  Created by Kazimir on 11.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
