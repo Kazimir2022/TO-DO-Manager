b60e88999fd0f2621170fbd1037077b22f7e3254
Kazimir Makaranka <m2014ky@gmail.com>
2023-04-17 16:30:11 +0300

    add folder model, file Task, file taskStorage

---
+++ TO-DO-Manager/Model/Task.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  TO-DO-Manager
+//
+//  Created by Kazimir on 11.04.23.
+//
+
+import Foundation
+// Тип задачи
+enum TaskPriority  {
+    // обычная
+    case normal
+    // важная
+    case important
+}
+
+// Состояние задачи
+enum TaskStatus: Int {
+    // текущая/запланированная
+    case planned
+    // завершенная
+    case completed
+}
+
+// Требования к типу, описывающему сущность "Задача"
+protocol TaskProtocol {
+    var title: String { get set }
+    var type: TaskPriority  { get set }
+    var status: TaskStatus { get set }
+}
+
+// Сущность "Задача"
+struct Task: TaskProtocol {
+    var title: String
+    var type: TaskPriority
+    var status: TaskStatus
+}
---
+++ TO-DO-Manager/Model/TaskStorage.swift
@@ -0,0 +1,8 @@
+//
+//  TaskStorage.swift
+//  TO-DO-Manager
+//
+//  Created by Kazimir on 11.04.23.
+//
+
+import Foundation
